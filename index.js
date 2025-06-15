9848d01035664aba2e9a13f9737696ae47890481
yude <i@yude.jp>
2025-06-15 18:13:46 +0900

    Add modal

--- index.js
+++ index.js
@@ -50,11 +50,21 @@ formMantela.addEventListener('submit', async e => {
 
 	const gallery = document.getElementById("gallery");
 
+	const isVideo = (url) => /\.mp4$/i.test(url);
+
 	media.map(x => {
 		const wrapper = document.createElement("figure");
 		wrapper.style.margin = "1rem 0";
+		const caption = document.createElement("figcaption");
+		caption.textContent = x[1];
+		caption.style.fontSize = "0.9rem";
+		caption.style.marginTop = "0.5rem";
 
-		if (x[0].endsWith(".mp4")) {
+		if (isVideo(x[0])) {
+			const vidWrapper = document.createElement("div");
+			vidWrapper.style.position = "relative";
+			vidWrapper.style.cursor = "pointer";
+
 			const video = document.createElement("video");
 			video.src = x[0];
 			video.volume = 0;
@@ -62,21 +72,71 @@ formMantela.addEventListener('submit', async e => {
 			video.autoplay = true;
 			video.loading = "lazy";
 			video.style.width = "100%";
-			wrapper.appendChild(video);
+			vidWrapper.appendChild(video)
+
+			vidWrapper.addEventListener("click", () => {
+				const modal = document.getElementById("modal");
+				const modalImg = document.getElementById("modalImage");
+
+				modalImg.replaceWith(modalImg.cloneNode());
+				const modalObj = document.createElement("video");
+				modalObj.src = obj.data;
+				modalObj.style.Width = "100%";
+				modalObj.style.maxHeight = "60vh";
+				modalObj.style.border = "4px solid white";
+				modalObj.style.backgroundColor = "#fff";
+				modalObj.style.objectFit = "contain";
+				modalObj.controls = true;
+				modalObj.video = 0;
+				modalObj.setAttribute("loading", "lazy");
+				modalObj.id = "modalImage";
+
+				document.getElementById("modal").replaceChild(modalObj, document.getElementById("modalImage"));
+				modal.style.display = "flex";
+
+				const modalCaption = document.getElementById("modalCaption");
+				modalCaption.innerText = x[1];
+			});
+			
+			wrapper.appendChild(vidWrapper);
 		} else {
+			const objWrapper = document.createElement("div");
+			objWrapper.style.position = "relative";
+			objWrapper.style.cursor = "pointer";
+
 			const obj = document.createElement("object");
 			obj.data = x[0];
 			obj.loading = "lazy";
 			obj.style.width = "100%";
-			wrapper.appendChild(obj);
+			obj.style.height = "auto";
+			objWrapper.appendChild(obj);
+
+			objWrapper.addEventListener("click", () => {
+				const modal = document.getElementById("modal");
+				const modalImg = document.getElementById("modalImage");
+
+				modalImg.replaceWith(modalImg.cloneNode());
+				const modalObj = document.createElement("object");
+				modalObj.data = obj.data;
+				modalObj.style.Width = "100%";
+				modalObj.style.maxHeight = "60vh";
+				modalObj.style.border = "4px solid white";
+				modalObj.style.backgroundColor = "#fff";
+				modalObj.style.objectFit = "contain";
+				modalObj.setAttribute("loading", "lazy");
+
+				modalObj.id = "modalImage";
+				document.getElementById("modalImageWrapper").replaceChild(modalObj, document.getElementById("modalImage"));
+				modal.style.display = "flex";
+
+				const modalCaption = document.getElementById("modalCaption");
+				modalCaption.innerText = x[1];
+			});
+
+			wrapper.appendChild(objWrapper);
+			wrapper.appendChild(caption);
 		}
-		
-		const caption = document.createElement("figcaption");
-		caption.textContent = x[1];
-		caption.style.fontSize = "0.9rem";
-		caption.style.marginTop = "0.5rem";
-		wrapper.appendChild(caption);
-		
+
 		gallery.appendChild(wrapper);
 	});
 });
@@ -89,3 +149,7 @@ if (urlSearch.get('first')) {
 	urlMantela.value = urlSearch.get('first');
 	btnGenerate.click();
 }
+
+document.getElementById("modalClose").addEventListener("click", () => {
+	document.getElementById("modal").style.display = "none";
+});
